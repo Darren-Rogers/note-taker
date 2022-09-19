1cae07dd1bac1cfb8824c5ade49af879813b190b
Sreggie <darren.rogers06@gmail.com>
2022-09-19 09:46:00 -0400

    working through problem with css file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const fs = require('fs');
 const util = require('util');
-
+const {readAndAppend, readFromFile, writeToFile} = require('./helper/new')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,14 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 app.get('/notes', function(req,res){
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 app.listen(PORT, ()=>{
   console.log(`listeing at http://localhost:${PORT}`)
@@ -27,5 +27,13 @@ app.post('api/notes', function(req, res){
   const notes = req.body
   console.log(notes)
   const text = req.body.text;
+  if(notes){
+    var newNotes = {title, text};
+    readAndAppend(newNotes,'./db/db.json')
+    res.json('New Note added')
+  }else{
+    res.error(err);
+  }
  
 })
+module.exports = express
